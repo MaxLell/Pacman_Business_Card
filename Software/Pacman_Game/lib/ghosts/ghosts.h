6a918d3efb8032db9fc793a7c4092055fbfbbbca
Max <maxkoell@proton.me>
2025-04-13 20:41:14 +0200

    Minor refactoring

--- Software/Pacman_Game/lib/ghosts/ghosts.h
+++ Software/Pacman_Game/lib/ghosts/ghosts.h
@@ -10,30 +10,8 @@ public:
 
     void update();
 
-    virtual positionXY_t move(ctrlInput input, positionXY_t currentPosition) = 0;
-
-    void gotEaten();
-
-protected:
-    DataPool& dataPool;
-
 private:
     positionXY_t position;
+    DataPool& dataPool;
     
 };
-
-// Blinky: Aggressiver Geist
-class Blinky : public Ghost {
-public:
-    Blinky();
-    void move(DataPool& dataPool) override;
-};
-
-// Pinky: Hinterhältiger Geist
-class Pinky : public Ghost {
-public:
-    Pinky(positionXY_t startPosition);
-    void move(DataPool& dataPool) override;
-};
-
-// Weitere Geister (Inky, Clyde, Sue) können ähnlich implementiert werden.
