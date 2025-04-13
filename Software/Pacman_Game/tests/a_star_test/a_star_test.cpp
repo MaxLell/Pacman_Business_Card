23be2cf7f8eddfc9b4c99ddfc900662d77092851
Max <maxkoell@proton.me>
2025-04-13 17:32:32 +0200

    added the a* algorithm implementation and added an initial test

---
+++ Software/Pacman_Game/tests/a_star_test/a_star_test.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include "custom_assert.h"
+#include "custom_assert_mock.h"
+#include "common_types.h"
+#include "environment_generator.h"
+#include "AStar.hpp"
+
+
+#include "CppUTestExt/MockSupport.h"
+#include "CppUTest/TestHarness.h"
+
+// clang-format off
+TEST_GROUP(a_star_tests){
+    void setup(){
+        mock().checkExpectations();
+        mock().clear();
+    }
+    void teardown()
+    {
+        mock().checkExpectations();
+        mock().clear();
+    }
+};
+// clang-format on
+
+TEST(a_star_tests, test_a_star)
+{
+    constexpr int WORLD_SIZE_X = 20;
+    constexpr int WORLD_SIZE_Y = 20;
+    constexpr int WALL_Y = 10;
+    constexpr int WALL_GAP_X = 5;
+    const AStar::Vec2i START_POSITION = {0, 0};
+    const AStar::Vec2i TARGET_POSITION = {19, 19};
+
+    AStar::Generator generator;
+    generator.setWorldSize({WORLD_SIZE_X, WORLD_SIZE_Y}); // Set world size
+    generator.setHeuristic(AStar::Heuristic::euclidean);
+    generator.setDiagonalMovement(true);
+
+    // Create a wall in the middle of the grid
+    for (int x = 0; x < WORLD_SIZE_X; ++x) {
+        generator.addCollision({x, WALL_Y}); // Wall at y = WALL_Y
+    }
+
+    // Remove a gap in the wall to test the algorithm
+    generator.removeCollision({WALL_GAP_X, WALL_Y}); // Opening at (WALL_GAP_X, WALL_Y)
+
+    auto path = generator.findPath(START_POSITION, TARGET_POSITION); // Path from START_POSITION to TARGET_POSITION
+
+    // // Make sure that the last coordinate is the target
+    CHECK_EQUAL(path.back().x, TARGET_POSITION.x);
+    CHECK_EQUAL(path.back().y, TARGET_POSITION.y);
+}
+
+
+
